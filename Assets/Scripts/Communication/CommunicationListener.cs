80954b98c4bf9a66e953f266dc328db215d118b7
jacty <saej3101@usherbrooke.ca>
2024-02-10 23:57:02 -0500

    Communicate by reading python script std output

--- Assets/Scripts/Communication/CommunicationListener.cs
+++ Assets/Scripts/Communication/CommunicationListener.cs
@@ -3,21 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Drawing;
+using System.Threading.Tasks;
+using System;
 
 public class CommunicationListener : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float roll = 0f;
+    private float pitch = 0f;
+    private float yaw = 0f;
+
+    private System.Diagnostics.Process daemonProcess;
+    private Func<bool> processFunction;
+
     void Start()
     {
-        ReadString();
+        DirectoryInfo dir = Directory.GetParent(Application.dataPath);
+        string daemonPath = Path.Combine(dir.FullName, "Assets", "Scripts", "Communication", "communication-daemon.py");
+
+        daemonProcess = new System.Diagnostics.Process();
+
+        daemonProcess.StartInfo.FileName = "python";
+        daemonProcess.StartInfo.Arguments = daemonPath;
+        daemonProcess.StartInfo.RedirectStandardOutput = true;
+        daemonProcess.StartInfo.UseShellExecute = false;
+        daemonProcess.StartInfo.CreateNoWindow = true;
+
+        processFunction = new Func<bool>(() => daemonProcess.Start());
+
+        ProcessScriptOutput();
     }
 
-    static void ReadString()
+    void FixedUpdate()
     {
-        string path = "Assets/test.txt";
+        transform.rotation = Quaternion.Euler(pitch, yaw, roll);
+    }
 
-        StreamReader reader = new StreamReader(path);
-        Debug.Log(reader.ReadToEnd());
-        reader.Close();
+    async void ProcessScriptOutput()
+    {
+        bool started = await Task.Run(processFunction);
+        Debug.Log($"Process started : {started}");
+
+        string message;
+        do
+        {
+            message = await daemonProcess.StandardOutput.ReadLineAsync();
+            if (message != null)
+            {
+                ConvertMessage(message);
+                await Task.Delay(10);
+            }
+        } while (message != null);
+    }
+
+    void ConvertMessage(string message)
+    {
+        string[] rotations = message.Split(" ");
+        
+        if (rotations.Length > 1)
+        {
+            roll = ParseRotation(rotations[0]);
+            pitch = ParseRotation(rotations[1]);
+            yaw = ParseRotation(rotations[2]);
+
+            Debug.Log("roll " + roll);
+            Debug.Log("pitch " + pitch);
+            Debug.Log("yaw " + yaw);
+        }
+    }
+
+    float ParseRotation(string strRotation)
+    {
+        return float.TryParse(strRotation, out float rotation) ? rotation * Mathf.Rad2Deg : 0f;
     }
 }
--- Assets/Scripts/Communication/communication-daemon.py
+++ Assets/Scripts/Communication/communication-daemon.py
@@ -1,4 +1,7 @@
 import asyncio
+import threading
+import numpy as np
+
 from bleak import BleakClient
 from bleak import BleakScanner
 
@@ -9,6 +12,36 @@ class CommunicationDaemon:
 
     def __init__(self) -> None:
         self.last_message = ""
+        self.send_buffer = ['test1', 'test2', 'test3']  # list of messages to send
+
+
+    async def receive_message(self, client):
+        while True:
+            message = await client.read_gatt_char(CHARACTERISTIC_UUID)
+            # print("Model Number: {0}".format("".join(map(chr, message))))
+            message = str(message, 'UTF-8')
+            print(f"Message received: {message}")
+
+            if message != self.last_message:
+                self.last_message = message
+                # write message to file
+                try:
+                    with open('message.txt', 'w') as file:
+                        file.write(message)
+                except:
+                    print('Could not write to file')
+                # with open('message.txt', 'w') as file:
+                #     file.write(message)
+
+            # print()
+            print(f"Message: {message}")
+
+
+    async def send_message(self, client):
+        while True:
+            message = 'test1234'
+            await client.write_gatt_char(CHARACTERISTIC_UUID, message.encode('UTF-8'))
+
 
     async def connect_to_sleeve(self):
         devices = await BleakScanner.discover()
@@ -19,21 +52,32 @@ class CommunicationDaemon:
         
         async with BleakClient(address) as client:
             await client.is_connected()
+
             print('CONNECTED')
+            receive_task = asyncio.create_task(self.receive_message(client))
+            # send_task = asyncio.create_task(self.send_message(client))
 
-            while True:
-                message = await client.read_gatt_char(CHARACTERISTIC_UUID)
-                message = str(message, 'UTF-8')
+            await receive_task
+            # await send_task
+            # t1 = threading.Thread(target=self.receive_message, args=(client,))
+            # # t2 = threading.Thread(target=self.send_message, args=(client,))
 
-                if message != self.last_message:
-                    self.last_message = message
-                    # write message to file
-                    with open('message.txt', 'w') as file:
-                        file.write(message)
+            # t1.start()
+            # # t2.start()
 
-                print()
-                print(f"Message: {message}")
+            # t1.join()
+            # t2.join()
 
-comm = CommunicationDaemon()
 
-asyncio.run(comm.connect_to_sleeve())
+
+# comm = CommunicationDaemon()
+# asyncio.run(comm.connect_to_sleeve())
+i = -2*np.pi    
+while True:
+    i += 0.1
+    if i > 2*np.pi:
+        i = -2*np.pi
+        
+    print(f'-1.07 {i} -0.91')
+
+
