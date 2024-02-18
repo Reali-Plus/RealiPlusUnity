007db972aef0bbd8cabced93648d5953ceae5931
Lolomimi99 <laumil99@hotmail.com>
2024-02-18 09:52:37 -0500

    Rotation Index

--- Assets/Scripts/DataConversion.cs
+++ Assets/Scripts/DataConversion.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class DataConversion : MonoBehaviour
 {
-    //[SerializeField] List<GameObject> Hand = new List<GameObject>();
+    [SerializeField] private GameObject indexFinger;
+    [SerializeField] private float vitesseRotation = 1f;
+    [SerializeField] private Vector3 receiveData = new Vector3(); //ici je vais recevoir une coordonnee
 
-    public Vector3 receiveData = new Vector3(); //ici je vais recevoir une coordonnée
-    public GameObject indexFinger;
+    //[SerializeField] private List<GameObject> fingers;
+    //[SerializeField] private List<Vector3> receiveData = new List<Vector3>(); 
 
     public struct HandData
     {
@@ -21,23 +23,31 @@ public class DataConversion : MonoBehaviour
 
     HandData handData = new HandData();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         handData.Index = receiveData;
-        indexFinger.transform.position = handData.Index;
+        Quaternion rotationVoulue = Quaternion.LookRotation(receiveData.normalized);
+        indexFinger.transform.rotation = Quaternion.Slerp(indexFinger.transform.rotation, rotationVoulue, Time.deltaTime * vitesseRotation);
         print(handData.Index);
-    }
-    public void SetFingerPosition(Vector3 newPosition)
-    {
-        handData.Index = newPosition;
-        //indexFinger.transform.position = handData.Index;
-    }
 
+        //if (receiveData.Count == fingers.Count)
+        //{
+        //    handData.Thumb = receiveData[0];
+        //    handData.Index = receiveData[1];
+        //    handData.Middle = receiveData[2];
+        //    handData.Ring = receiveData[3];
+        //    handData.Pinky = receiveData[4];
+
+        //    for (int i = 0; i < fingers.Count; i++)
+        //    {
+        //        Quaternion rotationVoulue = Quaternion.LookRotation(receiveData[i].normalized);
+        //        fingers[i].transform.rotation = Quaternion.Slerp(fingers[i].transform.rotation, rotationVoulue, Time.deltaTime * vitesseRotation);
+        //    }
+        //}
+        //print("Thumb Position: " + handData.Thumb);
+        //print("Index Position: " + handData.Index);
+        //print("Middle Position: " + handData.Middle);
+        //print("Pinky Position: " + handData.Pinky);
+    }
 }
