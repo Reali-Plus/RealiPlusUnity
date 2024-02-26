8eaceb60429cfe930d3805b765dd5c66b763c542
jacty <saej3101@usherbrooke.ca>
2024-02-26 01:11:05 -0500

    Demo WIP

--- Assets/Scripts/Communication/SocketCommunication.cs
+++ Assets/Scripts/Communication/SocketCommunication.cs
@@ -15,6 +15,7 @@ public class SocketCommunication : MonoBehaviour
     private float roll = 0f;
     private float pitch = 0f;
     private float yaw = 0f;
+    //private Vector3 acceleration;
 
 
     void Start()
@@ -57,12 +58,15 @@ public class SocketCommunication : MonoBehaviour
 
     void FixedUpdate()
     {
+        // transform.Translate(acceleration * Time.fixedDeltaTime);
+
         if (transform.rotation.eulerAngles.x == pitch && transform.rotation.eulerAngles.y == yaw && transform.rotation.eulerAngles.z == roll)
         {
             return;
         } 
-     
-        transform.rotation = Quaternion.Euler(pitch, yaw, roll);
+        
+        transform.rotation *= Quaternion.Euler(pitch * Time.fixedDeltaTime, yaw * Time.fixedDeltaTime, roll * Time.fixedDeltaTime);
+        
     }
 
     void OnApplicationQuit()
@@ -76,20 +80,27 @@ public class SocketCommunication : MonoBehaviour
 
     void ConvertMessage(string message)
     {
-        string[] rotations = message.Split(" ");
-
-        if (rotations.Length > 1)
+        Debug.Log(message);
+        string[] data = message.Split(" ");
+           
+        // TODO : Check if the message is valid
+        if (data.Length >= 3)
         {
-            roll = ParseRotation(rotations[0]);
-            pitch = ParseRotation(rotations[1]);
-            yaw = ParseRotation(rotations[2]);
+            roll = ParseRotation(data[0]);
+            pitch = ParseRotation(data[1]);
+            yaw = ParseRotation(data[2]);
+            /*float accX = float.TryParse(data[0], out float acc) ? acc * 9.8f : 0f;
+            float accY = float.TryParse(data[1], out acc) ? acc * 9.8f : 0f;
+            float accZ = float.TryParse(data[2], out acc) ? (acc - 1) * 9.8f  : 0f;
+            acceleration = new Vector3(accX, accY, accZ);*/
 
-            Debug.Log("roll " + roll + "pitch " + pitch + "yaw " + yaw);
+            //Debug.Log(acceleration);
+            Debug.Log("roll " + roll + " pitch " + pitch + " yaw " + yaw);
         }
     }
 
     float ParseRotation(string strRotation)
     {
-        return float.TryParse(strRotation, out float rotation) ? rotation * Mathf.Rad2Deg : 0f;
+        return float.TryParse(strRotation, out float rotation) ? rotation : 0f;
     }
 }
