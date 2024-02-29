0f2f5389c43ef849a0217969cc64ff492db9cc5c
Lolomimi99 <laumil99@hotmail.com>
2024-02-29 14:52:57 -0500

    ajout scene de test

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -7,6 +7,7 @@ public class DetectCollisions : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         print("COLLISIONS with: "+ gameObject.name);
+        Debug.DrawRay(collision.contacts[0].point, Vector3.up, Color.blue, 10.0f);
 
         //foreach (ContactPoint contact in collision.contacts)
         //{
