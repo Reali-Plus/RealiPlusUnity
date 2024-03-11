c41b36983dc7e6a24384a48a9a856a87c92df066
Lolomimi99 <laumil99@hotmail.com>
2024-03-11 17:37:12 -0400

    Ajout de la struct feedback

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
+    struct Feedback
+    {
+        // pour la rétroaction
+        public List<GameObject> fingersOnCollision;   // liste des doigt(s) en jeux dans la collision
+        public int intensity;   // intensité/force 
+        public int textureCoeff;   // chiffre/coefficient texture (largeur contact)
+        
+        // pour la restriction
+        public bool isOpen;  // ouvert/fermé pour la restriction
+        public int jointsPosition;   // position des joints // pas sur de son format vecteur?
+
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         print("COLLISIONS with: "+ gameObject.name);
-        Debug.DrawRay(collision.contacts[0].point, Vector3.up, Color.blue, 10.0f);
+        //Debug.DrawRay(collision.contacts[0].point, Vector3.up, Color.blue, 10.0f);
+    }
 
-        //foreach (ContactPoint contact in collision.contacts)
-        //{
-        //    print("COLLISIONS:"+ contact);
-        //}
+    // Lorsqu'il n'y a plus de collision on remet 
+    private void OnCollisionExit(Collision collision)
+    {
+        Feedback feedback = new Feedback();
+
+        feedback.fingersOnCollision = null;
+        feedback.intensity = 0;
+        feedback.textureCoeff = 0;
+
+        feedback.isOpen = true;
+        feedback.jointsPosition = 0;
     }
 }
