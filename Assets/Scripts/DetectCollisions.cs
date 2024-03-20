922e085956ff2f0bc9609ab89729a333097f9087
Lolomimi99 <laumil99@hotmail.com>
2024-03-20 11:41:12 -0400

    changement Find game object
    # modification pour trouver le gameObject,
    Find()-> FindGameObjectWithTag()

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -6,10 +6,12 @@ public class DetectCollisions : MonoBehaviour
 {
     private Dictionary<int, GameObject> fingerIds = new Dictionary<int, GameObject>();
 
+
     struct Feedback
     {
         // pour la rétroaction
         public List<int> fingersOnCollisionIds;     // liste des ID en collision
+        //public Dictionary<int, >                    //dictionnaire de l'id et de la position (coordonnees)
         public int intensity;                       // intensité/force 
         public int textureCoeff;                    // chiffre/coefficient texture (largeur contact)
         
@@ -20,12 +22,11 @@ public class DetectCollisions : MonoBehaviour
 
     private void Start()
     {
-        //voir si c'est la meilleure facon de le mapper
-        fingerIds.Add(0, GameObject.Find("Thumb_3"));
-        fingerIds.Add(1, GameObject.Find("Finger1_4"));
-        fingerIds.Add(2, GameObject.Find("Finger2_4"));
-        fingerIds.Add(3, GameObject.Find("Finger3_4"));
-        fingerIds.Add(4, GameObject.Find("Finger4_4"));
+        fingerIds.Add(0, GameObject.FindGameObjectWithTag("Thumb"));
+        fingerIds.Add(1, GameObject.FindGameObjectWithTag("Index"));
+        fingerIds.Add(2, GameObject.FindGameObjectWithTag("Middle"));
+        fingerIds.Add(3, GameObject.FindGameObjectWithTag("Ring"));
+        fingerIds.Add(4, GameObject.FindGameObjectWithTag("Pinky"));
     }
 
     void OnCollisionEnter(Collision collision)
@@ -37,18 +38,17 @@ public class DetectCollisions : MonoBehaviour
         {
             //Debug.Log("COLLISION WITH: " + gameObject.name + "\n At this point:" + contact.point);
             int fingerId = GetFingerIdFromGameObject(gameObject);
-
             if (fingerId != -1)
             {
                 feedback.fingersOnCollisionIds.Add(fingerId);
             }
         }
-
         // Affichage du contenu de la liste
         foreach (int id in feedback.fingersOnCollisionIds)
         {
             Debug.Log("ID EN COLLISION: " + id);
         }
+        //print((feedback.fingersOnCollisionIds).Count);
     }
 
     // Lorsqu'il n'y a plus de collision on remet 
