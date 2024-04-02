c919265c89a4bb9c30d96860bbc1932712707abe
Laurence <72349725+lolomimi99@users.noreply.github.com>
2024-04-02 11:03:52 -0400

    Detect collisions (#3)
    * detection de base
    
    * ajout scene de test
    
    * Ajuster le contactOffet pour collision
    
    * Ajout de la struct feedback
    
    * Add contacts point V1
    
    +add collision.contacts qui renvoit les coordonnées du points de contact mais non conseillé (mem garbage)
    
    * change gameObject to ID
    
    * changement Find game object
    
    # modification pour trouver le gameObject,
    Find()-> FindGameObjectWithTag()
    
    * Modification du tag sur le prefab
    
    * Fix tag multiple sous finger
    
    * Changer Dictionnaire à liste
    
    * Cleaner
    
    * PR comments
    
    * suite pr
    
    * Rethink detection collision
    
    +repenser la detection pour etre capable d'envoyer une seule liste vers la communication (bas-niveau/elect)
    **Trouver un moyen de cleaner la liste lors de la sortie de la collision
    
    * Add OnCollisionExit logic
    
    ***Push with print for testing***
    
    * Remove print
    
    * Modification du prefab
    
    * erreur de nommage

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private DetectCollisions parentScript;
+
+    public void Initialize(DetectCollisions parent)
+    {
+        parentScript = parent;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        parentScript.HandleCollision(gameObject);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        parentScript.ExitCollision();
+    }
+}
