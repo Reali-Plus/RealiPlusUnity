9b650b51eaa90940a786b8f47796afb0cd613029
Lolomimi99 <laumil99@hotmail.com>
2024-04-09 11:48:40 -0400

    Add color display for test

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,6 +6,8 @@ public class CollisionHandler : MonoBehaviour
 {
     private DetectCollisions parentScript;
     private SocketCommunication socketCommunication;
+    public Color collisionColor = Color.red;
+    public Color triggerColor = Color.blue;
 
     private void Start()
     {
@@ -20,24 +22,37 @@ public class CollisionHandler : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         print("OnCollisionENTER");
+        DisplayCollider(collision.collider, collisionColor);
         parentScript.UpdateFeedback(gameObject, true, true);
     }
 
     private void OnCollisionExit(Collision collision)
     {
         print("OnCollisionEXIT");
+        DisplayCollider(collision.collider, triggerColor);
         parentScript.UpdateFeedback(gameObject, true, false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         print("OnTriggerEnter");
+        DisplayCollider(other, triggerColor);
         parentScript.UpdateFeedback(gameObject, true, false);
     }
 
     private void OnTriggerExit(Collider other)
     {
         print("OnTriggerExit");
+        DisplayCollider(other, Color.white);
         parentScript.UpdateFeedback(gameObject, false, false);
+    }
+
+    private void DisplayCollider(Collider collider, Color color)
+    {
+        MeshRenderer colliderRenderer = collider.GetComponent<MeshRenderer>();
+        if (colliderRenderer != null)
+        {
+            colliderRenderer.material.color = color;
+        }
     }
 }
