8e3e34734496567f8649ef1c8426eabaee348aee
Lolomimi99 <laumil99@hotmail.com>
2024-04-10 10:13:05 -0400

    Clean for PR

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
     private DetectCollisions parentScript;
     private SocketCommunication socketCommunication;
-    public Color collisionColor = Color.red;
-    public Color triggerColor = Color.blue;
 
     private void Start()
     {
-        //socketCommunication = GameObject.FindGameObjectWithTag("SleeveCommunication").GetComponent<SocketCommunication>();
+        socketCommunication = GameObject.FindGameObjectWithTag("SleeveCommunication").GetComponent<SocketCommunication>();
     }
 
     public void Initialize(DetectCollisions parent)
@@ -21,38 +17,21 @@ public class CollisionHandler : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        print("OnCollisionENTER");
-        DisplayCollider(collision.collider, collisionColor);
         parentScript.UpdateFeedback(gameObject, true, true);
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        print("OnCollisionEXIT");
-        DisplayCollider(collision.collider, triggerColor);
         parentScript.UpdateFeedback(gameObject, true, false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        print("OnTriggerEnter");
-        DisplayCollider(other, triggerColor);
         parentScript.UpdateFeedback(gameObject, true, false);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        print("OnTriggerExit");
-        DisplayCollider(other, Color.white);
         parentScript.UpdateFeedback(gameObject, false, false);
-    }
-
-    private void DisplayCollider(Collider collider, Color color)
-    {
-        MeshRenderer colliderRenderer = collider.GetComponent<MeshRenderer>();
-        if (colliderRenderer != null)
-        {
-            colliderRenderer.material.color = color;
-        }
     }
 }
