f4a974adba188fd231b1519babee6b22839ecb4d
Lolomimi99 <laumil99@hotmail.com>
2024-09-19 14:35:17 -0400

    Highlight cube

---
+++ Assets/Scripts/Game/EscapeRoom/CubeButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeButton : MonoBehaviour
+{
+    [SerializeField]Color defaultColor;
+    [SerializeField]Color highlightColor;
+    [SerializeField]float resetDelay =.25f;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        ResetButton();
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<MeshRenderer>().material.color = highlightColor;
+        Invoke("ResetButton", resetDelay);
+    }
+    void ResetButton()
+    {
+        GetComponent<MeshRenderer>().material.color = defaultColor;
+    }
+
+}
---
+++ Assets/Scripts/Game/EscapeRoom/SimonManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class SimonManager : MonoBehaviour
+{
+    [SerializeField] CubeButton[] cubes; // Tableau pour les cubes
+
+    private void Start()
+    {
+      
+    }
+
+    //public void HighlightCube(int index)
+    //{
+    //    if (index >= 0 && index < cubes.Length)
+    //    {
+    //        cubes[index].Highlight(); // Appelle la méthode Highlight() du cube sélectionné
+    //    }
+    //}
+
+    //// Exemple d'une fonction pour simuler une séquence de couleurs
+    //public void PlaySequence(int[] sequence)
+    //{
+    //    foreach (int index in sequence)
+    //    {
+    //        HighlightCube(index); // Surbrillance des cubes en fonction de la séquence
+    //    }
+    //}
+}
+
