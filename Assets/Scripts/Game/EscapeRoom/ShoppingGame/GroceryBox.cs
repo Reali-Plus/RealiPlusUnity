f0ab7ff67f2808c2785dd30ac2854bae144239f0
Lolomimi99 <laumil99@hotmail.com>
2024-10-22 13:57:44 -0400

    Added GameManager

---
+++ Assets/Scripts/Game/EscapeRoom/ShoppingGame/GroceryBox.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroceryBox : MonoBehaviour
+{
+    private ShopManager shopManager;
+    private GroceryListHandler groceryListHandler;
+    private List<GameObject> collectedItems = new List<GameObject>();
+
+    private void Start()
+    {
+        shopManager = GameObject.FindGameObjectWithTag("ShopManager").GetComponent<ShopManager>();
+        groceryListHandler = GameObject.FindGameObjectWithTag("ShopManager").GetComponent<GroceryListHandler>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!shopManager.alreadyWon)
+        {
+            if (groceryListHandler.GetGroceryList().Contains(other.gameObject))
+            {
+                collectedItems.Add(other.gameObject);
+                groceryListHandler.MarkItemAsCorrect(other.gameObject);
+            }
+            else
+            {
+                collectedItems.Add(other.gameObject);
+                groceryListHandler.MarkItemAsIncorrect(other.gameObject);
+            }
+            shopManager.CheckIfAllItemsCollected(collectedItems);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!shopManager.alreadyWon)
+        {
+            collectedItems.Remove(other.gameObject);
+            if (groceryListHandler.GetGroceryList().Contains(other.gameObject))
+            {
+                groceryListHandler.MarkItemAsCorrect(other.gameObject);
+            }
+            else
+            {
+                groceryListHandler.MarkItemAsIncorrect(other.gameObject);
+            }
+            shopManager.CheckIfAllItemsCollected(collectedItems);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/EscapeRoom/ShoppingGame/GroceryListHandler.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GroceryListHandler : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro groceryListText;
+    [SerializeField] private TextMeshPro incorrectItemsText;
+    private List<GameObject> groceryList = new List<GameObject>();
+    private List<GameObject> correctItems = new List<GameObject>();
+    private List<GameObject> incorrectItems = new List<GameObject>();
+
+    public void GenerateGroceryList(int nbrItemsInGroceryList, List<GameObject> allItems)
+    {
+        if (allItems.Count < nbrItemsInGroceryList)
+        {
+            Debug.LogWarning("Not enough items to select from!");
+            return;
+        }
+
+        List<GameObject> itemsToSelectFrom = new List<GameObject>(allItems);
+        groceryList = GetRandomItems(itemsToSelectFrom, nbrItemsInGroceryList);
+        DisplayGroceryList();
+    }
+
+    private List<GameObject> GetRandomItems(List<GameObject> sourceList, int itemCount)
+    {
+        List<GameObject> selectedItems = new List<GameObject>();
+        for (int i = 0; i < itemCount; i++)
+        {
+            int randomIndex = Random.Range(0, sourceList.Count);
+            selectedItems.Add(sourceList[randomIndex]);
+            sourceList.RemoveAt(randomIndex);
+        }
+        return selectedItems;
+    }
+
+    private void DisplayGroceryList()
+    {
+        string listText = "Items:\n";
+        foreach (GameObject item in groceryList)
+        {
+            if (correctItems.Contains(item))
+            {
+                listText += "- " + "<s>" + item.name + "</s>\n";
+            }
+            else
+            {
+                listText += "- " + item.name + "\n";
+            }
+        }
+        DisplayIncorrectItems();
+        groceryListText.text = listText;
+    }
+
+    private void DisplayIncorrectItems()
+    {
+        string incorrectListText = "Items incorrect:\n";
+        foreach (GameObject incorrectItem in incorrectItems)
+        {
+            incorrectListText += "<color=red>" + incorrectItem.name + "</color>\n"; 
+        }
+
+        incorrectItemsText.text = incorrectListText;
+    }
+
+    public void MarkItemAsCorrect(GameObject item)
+    {
+        if (!correctItems.Contains(item))
+        {
+            correctItems.Add(item);
+            DisplayGroceryList();
+        }
+        else
+        {
+            correctItems.Remove(item);
+            DisplayGroceryList();
+        }
+    }
+
+    public void MarkItemAsIncorrect(GameObject incorrectItem)
+    {
+        if (!incorrectItems.Contains(incorrectItem))
+        {
+            incorrectItems.Add(incorrectItem);
+            DisplayGroceryList();
+        }
+        else
+        {
+            incorrectItems.Remove(incorrectItem);
+            DisplayGroceryList();
+        }
+    }
+
+    public List<GameObject> GetGroceryList()
+    {
+        return groceryList;
+    }
+}
