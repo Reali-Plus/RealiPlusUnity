d79472b8b246fcc9099bac4570091064a5022d0b
jacty <saej3101@usherbrooke.ca>
2024-10-22 17:07:03 -0400

    Clean code

--- Assets/Scripts/Communication/HapticsData.cs
+++ Assets/Scripts/Communication/HapticsData.cs
@@ -1,26 +1,3 @@
-using System.Collections.Generic;
-
-/*public struct FingerFeedback
-{
-    public SensorID fingerId;
-    public bool retroactionResponse;
-    public bool restrictionResponse;
-
-    public FingerFeedback(SensorID fingerId, bool retroactionResponse, bool restrictionResponse)
-    {
-        this.fingerId = fingerId;
-        this.retroactionResponse = retroactionResponse;
-        this.restrictionResponse = restrictionResponse;
-    }
-
-    public static string BoolToString(bool value) => value ? "1" : "0";
-
-    public override string ToString()
-    {
-        return fingerId + BoolToString(retroactionResponse) + BoolToString(restrictionResponse);
-    }
-}*/
-
 public class HapticsData
 {
     public SensorID FingerId { get; private set; }
@@ -60,45 +37,4 @@ public class HapticsData
     {
         return FingerId + BoolToString(RetroactionResponse) + BoolToString(RestrictionResponse);
     }
-
-    // public List<FingerFeedback> FeedbackList { get; private set; }
-
-    /*public HapticsData()
-    {
-        FeedbackList = new List<FingerFeedback>();
-    }
-
-    public HapticsData(List<FingerFeedback> feedbacks)
-    {
-        FeedbackList = feedbacks;
-    }
-
-    public HapticsData(SensorID fingerId, bool retroactionResponse, bool restrictionResponse)
-    {
-        FeedbackList = new List<FingerFeedback>
-        {
-            new FingerFeedback(fingerId, retroactionResponse, restrictionResponse)
-        };
-    }
-
-    public void AddFeedback(FingerFeedback feedback)
-    {
-        FeedbackList.Add(feedback);
-    }
-
-    public void ClearFeedback()
-    {
-        FeedbackList.Clear();
-    }
-
-    public override string ToString()
-    {
-        string data = "";
-        for (int i = 0; i < FeedbackList.Count; i++)
-        {
-            data += FeedbackList[i].ToString() + " ";
-        }
-
-        return data;
-    }*/
 }
