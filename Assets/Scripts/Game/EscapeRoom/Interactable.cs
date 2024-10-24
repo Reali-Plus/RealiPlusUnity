c5876e5fe0fbdcb88d79f707cf0ee86487d1fb31
mill3003 <mill3003@usherbrooke.ca>
2024-10-24 09:17:22 -0400

    Update Assets/Scripts/Game/EscapeRoom/Interactable.cs
    Co-authored-by: Jacty Saenz <47018467+jactymilena@users.noreply.github.com>

--- Assets/Scripts/Game/EscapeRoom/Interactable.cs
+++ Assets/Scripts/Game/EscapeRoom/Interactable.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
-    [field: SerializeField] public bool IsInteractable { get; protected set; } = true;
+    [field: SerializeField] public bool CanInteractable { get; protected set; } = true;
 
     public abstract void Interact(Vector3 position);
 }
