80c180a6990e8c875b6a457bafeecfef71598ee5
mill3003 <mill3003@usherbrooke.ca>
2024-10-25 09:32:28 -0400

    Escape room Shopping Game (#17)
    * Added Assets
    
    * Added object for shop
    
    * Added random grocery list
    
    * dynamically show grocery list in the board
    
    * Try to drag object
    
    * Added move object and check grocery
    
    * Refactor gameLogic
    
    -> need to fix in unity the box collider + when wrong items are in the box...
    
    * Clean code + added onTriggerExit
    
    * Added alreadyWon bool
    
    * Fix victory criteria
    
    * Fix grocery list visual
    
    * Refactoring
    
    * Rename for french
    
    * Added wonObjet + fix scratch item
    
    * fix incorrect visual
    
    * Clean code
    
    * Move scripts and delete non utile asset
    
    * Added spaces
    
    ---------
    
    Co-authored-by: Lolomimi99 <laumil99@hotmail.com>

---
+++ Assets/Scripts/Game/EscapeRoom/MoveObject.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    private Camera mainCamera;
+    private bool isDragging = false;
+    private GameObject selectedObject;
+    private Vector3 offset;
+    private List<GameObject> moveableItems = new List<GameObject>();
+
+    public GameObject itemsInStore;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        PopulateItemList(itemsInStore.transform, moveableItems);
+    }
+
+    void Update()
+    {
+        HandleObjectDragging();
+    }
+
+    //TODO: change with the hand model
+    private void HandleObjectDragging()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (TryGetHitObject(out hit))
+            {
+                GameObject hitObject = hit.collider.gameObject;
+
+                if (moveableItems.Contains(hitObject))
+                {
+                    StartDragging(hitObject);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging && selectedObject != null)
+        {
+            selectedObject.transform.position = GetMouseWorldPosition() + offset;
+        }
+    }
+
+    private bool TryGetHitObject(out RaycastHit hit)
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        return Physics.Raycast(ray, out hit);
+    }
+
+    private void StartDragging(GameObject hitObject)
+    {
+        selectedObject = hitObject;
+        isDragging = true;
+        offset = selectedObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mainCamera.WorldToScreenPoint(selectedObject.transform.position).z;
+        return mainCamera.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void PopulateItemList(Transform parent, List<GameObject> itemList)
+    {
+        foreach (Transform child in parent)
+        {
+            itemList.Add(child.gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Game/EscapeRoom/ShopManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManager : MonoBehaviour
+{
+    [SerializeField] int nbrItemsInGroceryList = 3;
+
+    private GameObject itemsInStore;
+    private GameObject wonObject;
+    private GroceryListHandler groceryListHandler;
+    private List<GameObject> allItemsAvailable = new List<GameObject>();
+    
+    public bool alreadyWon = false;
+
+    private void Start()
+    {
+        groceryListHandler = GameObject.FindGameObjectWithTag("ShopManager").GetComponent<GroceryListHandler>();
+        wonObject = GameObject.FindGameObjectWithTag("Key");
+        itemsInStore = GameObject.FindGameObjectWithTag("ItemsInStore");
+
+        wonObject.SetActive(false);
+        alreadyWon = false;
+        allItemsAvailable.Clear();
+
+        PopulateItemList();
+        groceryListHandler.GenerateGroceryList(nbrItemsInGroceryList, allItemsAvailable);
+    }
+
+    private void PopulateItemList()
+    {
+        foreach (Transform child in itemsInStore.transform)
+        {
+            allItemsAvailable.Add(child.gameObject);
+        }
+    }
+
+    public void CheckIfAllItemsCollected(List<GameObject> collectedItems)
+    {
+        HashSet<GameObject> collectedSet = new HashSet<GameObject>(collectedItems);
+        HashSet<GameObject> groceryListSet = new HashSet<GameObject>(groceryListHandler.GetGroceryList());
+        if (!alreadyWon)
+        {
+            if (collectedItems.Count == groceryListHandler.GetGroceryList().Count) 
+            { 
+                if (collectedSet.SetEquals(groceryListSet))
+                {
+                     OnAllItemsCollected();
+                }
+                else
+                {
+                    Debug.Log("There are incorrect or missing items in the box!");
+                }
+            }
+        }
+    }
+
+    private void OnAllItemsCollected()
+    {
+        alreadyWon = true;
+        wonObject.SetActive(true);
+        foreach (Transform child in itemsInStore.transform)
+        {
+            //TODO: block interaction
+        }
+    }
+}
