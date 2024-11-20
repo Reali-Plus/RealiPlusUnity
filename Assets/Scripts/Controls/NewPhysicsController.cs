d2b72570355c19247deea5cd7870ffc38319e2c9
Jacty Saenz <47018467+jactymilena@users.noreply.github.com>
2024-11-19 19:26:02 -0500

    Adjust hand colliders (#28)
    * Added colliders and more
    
    * Added all colliders
    
    * Rg collision detection and physics penetration settings
    
    * Removed code that was added for test reasons

--- Assets/Scripts/Controls/NewPhysicsController.cs
+++ Assets/Scripts/Controls/NewPhysicsController.cs
@@ -14,10 +14,10 @@ public class NewPhysicsController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rigidBody.velocity = Vector3.zero;
         rigidBody.angularVelocity = Vector3.zero;
-        rigidBody.MovePosition(target.position);
         rigidBody.MoveRotation(target.rotation);
+        
+        rigidBody.velocity = (target.position - rigidBody.position) / Time.fixedDeltaTime;
     }
 
     private void OnDrawGizmos()
