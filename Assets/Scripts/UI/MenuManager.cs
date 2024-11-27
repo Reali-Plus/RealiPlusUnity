dbe9b76b899829d9cf030d4180f252044f40295f
Lolomimi99 <laumil99@hotmail.com>
2024-11-27 12:35:39 -0500

    Try fix init com

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -43,6 +43,7 @@ public class MenuManager : MonoBehaviour
         {
             menu.SetActive(false);
             DeactivateWarning();
+            sleeveCommunication.InitilializeCommunication();
         }
         else
         {
