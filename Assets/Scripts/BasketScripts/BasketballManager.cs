b38faae48cb5765c610c55a4f7049a5f12aa0385
jacty <saej3101@usherbrooke.ca>
2024-11-28 12:31:04 -0500

    Change angle of basketball game

--- Assets/Scripts/BasketScripts/BasketballManager.cs
+++ Assets/Scripts/BasketScripts/BasketballManager.cs
@@ -5,11 +5,25 @@ public class BasketballManager : MiniGameManager
     [SerializeField]
     private GrabbableBall grabbableBall;
 
+    [SerializeField] private GameObject playerHead;
+
+    [SerializeField] private Vector3 playerHeadAngle = Vector3.zero;
+
+    private Vector3 lastHeadAngle = Vector3.zero;
+
     public override void Initialize() { }
-    protected override void StartGame(){ }
+
+    protected override void StartGame()
+    {
+        lastHeadAngle.x = playerHead.transform.localEulerAngles.x;
+        lastHeadAngle.y = playerHead.transform.localEulerAngles.y;
+        lastHeadAngle.z = playerHead.transform.localEulerAngles.z;
+        playerHead.transform.localEulerAngles = playerHeadAngle;
+    }
 
     protected override void ResetGame()
     {
+        playerHead.transform.localEulerAngles = lastHeadAngle;
         grabbableBall.ResetGrabbableBall();
     }
 }
